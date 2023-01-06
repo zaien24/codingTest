deff758ebf76167ef68f6310d8d3712b365e15a7
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-01-06 15:12:51 +0900

    p101 04 정렬 > 문제 15 수 정렬하기 1

---
+++ src/doitcoding/P2750_수정렬하기.java
@@ -0,0 +1,31 @@
+package doitcoding;
+
+import java.util.Scanner;
+
+public class P2750_수정렬하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		//버블 정렬을 구현하는 영역
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = 0; j < N - 1 - i; j++) {
+				if (A[j] > A[j+1]) {
+					int temp = A[j];
+					A[j] = A[j+1];
+					A[j+1] = temp;
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println("정렬 : " + A[i]);
+		}
+	}
+
+}
