0aa1bac828c9d42cf61de25cd8e0aa85a43b634c
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-01-15 23:16:05 +0900

    코딩테스트 - 패스트캠프_자바 > part3> ch02 > 01 완전탐색(Brute Force) - 백준 15651

--- src/fastCamp1/P15651.java
+++ src/fastCamp1/P15651.java
@@ -25,17 +25,32 @@ public class P15651 {
 	// 만약 M 개를 전부 고름 => 조건에 맞는 탐색을 한 가지 성공한 것!
 	// 아직 M 개를 고르지 않음 => k 번쨰부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다. 
 	private static void rec_func(int k) {
+		
+		System.out.println("분기 처리 전");
+		System.out.println("분기 처리 전 k : " + k);
+		
 		if (k == M + 1) { // 다 골랐다!
+			System.out.println("분기 처리 후1");
+			System.out.println("분기 처리 후1 k : " + k);
+			System.out.println("분기 처리 후1 M : " + M);
+			
 			// selected[1...M] 배열이 새롭게 탐색된 겨로가 
 			for (int i=1;i<=M;i++) {
+				System.out.println("분기 처리 후1 i : " + i);
 				sb.append(selected[i]).append(' ');		
 			}
 			sb.append('\n');			
 		} else {
+			System.out.println("분기 처리 후2");
 			for (int cand=1;cand<=N;cand++) {
+				System.out.println("분기 처리 후2 cand : " + cand);
+				System.out.println("분기 처리 후2 selected : " + selected[k]);
 				selected[k] = cand;
 				// k+1 번 ~ M 번 을 모두 탐색하는 일을 해야 하는 상황
+				System.out.println("분기 처리 후2 k: " + k);
+				System.out.println("분기 처리 후2 selected2 : " + selected[k]);				
 				rec_func(k+1);
+				System.out.println("재귀 끝");
 				selected[k] = 0;
 			}
 		}
