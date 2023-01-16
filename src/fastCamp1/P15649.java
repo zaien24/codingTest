3d05a5f873e8cefddceec7d87721f938c0b73dfa
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-01-16 11:38:02 +0900

    코딩테스트 - 패스트캠프_자바 > part3> ch02 > 01 완전탐색(Brute Force) - 백준 15649 방법1 (for문)

---
+++ src/fastCamp1/P15649.java
@@ -0,0 +1,106 @@
+package fastCamp1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15649 {
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N, M;
+	static int[] selected;
+	
+	
+	private static void input() {
+		FastReader scan = new FastReader();	
+		N = scan.nextInt();
+		M = scan.nextInt();
+		selected = new int[M + 1];
+	}
+	
+	private static void rec_func(int k) {		
+		//출력
+		if (k == M + 1 ) {
+			for (int i=1;i<=M;i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int cand=1;cand<=N;cand++) {
+				boolean isUsed = false;
+				for (int i=1;i<k;i++) {
+					if (cand == selected[i]) {
+						isUsed = true;
+					}
+				}
+				if (!isUsed) {
+					selected[k] = cand;				
+					rec_func(k+1);
+					selected[k] = 0;	
+				}				
+			}				
+		}
+		//저장
+	}
+	
+	
+	public static void main(String[] args) {
+		input();
+		
+		rec_func(1);		
+		System.out.println(sb);
+	}
+
+	
+
+	// 입출력 클래스 
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+        	String str = "";
+        	try {
+        		str = br.readLine();
+        	} catch (IOException e) {
+        		e.printStackTrace();
+        	}
+        	return str;
+        }
+	}
+}
