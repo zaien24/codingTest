e9bd0652a187e8da4394704537a54ca4392d91e0
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-02-07 19:19:32 +0900

    코딩테스트 - 기본양식

---
+++ src/fastCamp1/Basic.java
@@ -0,0 +1,72 @@
+package fastCamp1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Basic {
+	static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+	
+    
+    static void input() {
+    	
+    }
+    
+    
+    public static void main(String[] args) {
+    	input();
+    	    	
+    }
+   
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
