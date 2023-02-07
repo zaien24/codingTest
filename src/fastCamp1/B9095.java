e96329fdbf565aeb9434cf5bd63b41eaaeb3810f
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-02-07 19:22:39 +0900

    [B9095] 1, 2, 3 더하기 - 제출

--- src/fastCamp1/B9095.java
+++ src/fastCamp1/B9095.java
@@ -14,19 +14,23 @@ public class B9095 {
     static StringBuilder sb = new StringBuilder();
 
     static int[] Dy;
-
-    static void preprocess() {
+    
+    static void preprocess(){
         Dy = new int[15];
         // 초기값 구하기
-        /* TODO */
+        Dy[1] = 1;
+        Dy[2] = 2;
+        Dy[3] = 4;
 
         // 점화식을 토대로 Dy 배열 채우기
-        /* TODO */
+        for (int i = 4; i <= 11; i++){
+            Dy[i] = Dy[i - 1] + Dy[i - 2] + Dy[i - 3];
+        }
     }
 
     static void pro() {
         int T = scan.nextInt();
-        for (int tt = 1; tt <= T; tt++) {
+        for (int tt = 1; tt <= T; tt++){
             int N = scan.nextInt();
             sb.append(Dy[N]).append('\n');
         }
