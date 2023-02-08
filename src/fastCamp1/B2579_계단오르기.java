220453e4c390959532852e73ab9d40308bea3a95
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-02-08 17:51:41 +0900

    [B2579] 계단 오르기 - 제출

--- src/fastCamp1/B2579_계단오르기.java
+++ src/fastCamp1/B2579_계단오르기.java
@@ -28,15 +28,20 @@ public class B2579_계단오르기 {
 
     static void pro() {
         // 초기값 구하기
-        /* TODO */
+    	Dy[1][0] = 0;
+        Dy[1][1] = A[1];
+        
+        if (N >= 2){
+            Dy[2][0] = A[2];
+            Dy[2][1] = A[1] + A[2];
+        }
 
         // 점화식을 토대로 Dy 배열 채우기
-        /* TODO */
-        
-        // Dy배열로 정답 계산하기
-        int ans = /* TODO */
-            
-        System.out.println(ans);
+        for (int i = 3; i <= N; i++){
+            Dy[i][0] = Math.max(Dy[i - 2][0] + A[i], Dy[i - 2][1] + A[i]);
+            Dy[i][1] = Dy[i - 1][0] + A[i];
+        }
+        System.out.println(Math.max(Dy[N][0], Dy[N][1]));
     }
 
     public static void main(String[] args) {
