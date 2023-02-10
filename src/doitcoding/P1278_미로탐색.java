2f9f4c47bd21ea736b41b1f90e5a363cbc6fc072
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-02-10 20:23:33 +0900

    [B2178] 미로탐색 - 제출

--- src/doitcoding/P1278_미로탐색.java
+++ src/doitcoding/P1278_미로탐색.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class P1278_미로탐색 {
-	
+	//2178 
 	static int[] dx = {0, 1, 0, -1};
 	static int[] dy = {1, 0, -1, 0};
 	static boolean[][] visited;
