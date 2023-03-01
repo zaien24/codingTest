d1559ebbb9f596e9527899decb75881a1de98664
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2023-03-01 18:35:01 +0900

    동적프로그래밍_B15988_123더하기3

--- src/fastCamp1/동적프로그래밍_B15988_123더하기3.java
+++ src/fastCamp1/동적프로그래밍_B15988_123더하기3.java
@@ -12,52 +12,47 @@ import java.util.StringTokenizer;
 public class 동적프로그래밍_B15988_123더하기3 {
 	static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-	
+
     static int Q, MOD = 1000000009;
     static int[] Dy;
-    
+
     static void input() {
-    	Q = scan.nextInt();
+        Q = scan.nextInt();
     }
-    
+
     static void preprocess() {
-    	// Dy[i] := i 를 1, 2,3 의 합으로 표현하는 방법
-    	// Dy[0] 은 사실 문제 정의 상 말이 안되지만, 이후 점화식을 간단하게 적용하기 위해서 
-    	// "아무것도 안 더한다"는 것을 하나의 경루로 생각해줘서 1을 적어준다. 
-    	// 이를 통해서 Dy[1,2,3]에 대해서도 일관성 있는 점화식을 작성할 수 있기 때문이다. 
-    	// 이게 불편다하면 Dy[1,2,3]에 대해 모두 갑을 적어주고 시작해도 상관없다. 
-    	Dy[0] = 1;
-    	for (int i = 1; i < 1000000; i++) {
-    		Dy[i] = Dy[i-1];
-    		if (i - 2 >= 0) {
-    			Dy[i] += Dy[i-2];
-    			
-    		}
-    		Dy[i] %= MOD;
-    		if (i - 3 >= 0) {
-    			Dy[i] += Dy[i-3];
-    		}
-    		Dy[i] %= MOD;
-    	}
+        // Dy[i] := i 를 1, 2, 3의 합으로 표현하는 방법
+        // Dy[0] 은 사실 문제 정의 상 말이 안되지만, 이후 점화식을 간단하게 적용하기 위해서
+        // "아무것도 안 더한다"는 것을 하나의 경우로 생각해줘서 1을 적어준다.
+        // 이를 통해서 Dy[1, 2, 3]에 대해서도 일관성있는 점화식을 작성할 수 있기 때문이다.
+        // 이게 불편하다면 Dy[1, 2, 3] 에 대해 모두 값을 적어주고 시작해도 상관없다.
+        Dy[0] = 1;
+        for (int i = 1; i <= 1000000; i++) {
+            Dy[i] = Dy[i - 1];  // 마지막에 1을 더하는 경우의 수
+            if (i - 2 >= 0) Dy[i] += Dy[i - 2];  // 마지막에 2을 더하는 경우의 수
+            Dy[i] %= MOD;
+            if (i - 3 >= 0) Dy[i] += Dy[i - 3];  // 마지막에 3을 더하는 경우의 수
+            Dy[i] %= MOD;
+        }
     }
-    
+
     static void pro() {
-    	Dy = new int[1000005];
-    	preprocess();
-    	while (Q-- > 0) {
-    		int x = scan.nextInt();
-    		sb.append(Dy[x]).append('\n');
-    	}
-    	System.out.println(sb);
+        Dy = new int[1000005];
+        preprocess();
+        while (Q-- > 0) {
+            int x = scan.nextInt();
+            sb.append(Dy[x]).append('\n');
+        }
+        System.out.println(sb);
     }
-    
+
     public static void main(String[] args) {
-    	input();
-    	pro();
-    	    	
+        input();
+        pro();
     }
-   
-	static class FastReader {
+
+
+    static class FastReader {
         BufferedReader br;
         StringTokenizer st;
 
