999d36d30a804f8198c6725271637bafce47618f
yuyuhi <zaien24@naver.com>
2024-03-14 22:27:00 +0900

    240314_9663 시간 초과됨
    백트레킹은 모든 방법을 일일이 확인하는방법
    정답이 21억이 넘는다는것은
    수행 연산시간도 21억을 넘는것이기때문에
    int 범위라는것을 간접적으로 유추할 수 있다.

---
+++ src/baekjoon/C9663.java
@@ -0,0 +1,87 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class C9663 {
+	
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+	static void input() {
+		N = scan.nextInt();		
+		col = new int[N + 1];
+	}
+
+	static int N, ans;
+	static int[] col;
+	
+	static boolean attackable(int r1, int c1, int r2, int c2) {
+		if (c1 == c2) return true;
+		if (r1 - c1 == r2 - c2) return true;
+		if (r1 + c1 == r2 + c2) return true;
+		return false;
+	}
+	
+	static boolean validity_check() {
+		for (int i=1;i<=N;i++) {
+			// (i, col[i])
+			for (int j=1;j<i;j++) {
+				// (j, col[j])
+				if (attackable(i, col[i], j, col[j])) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	static void rec_func(int row) {
+		if (row == N + 1) {
+			if (validity_check()) {
+				ans++;
+			}
+		} else {
+			for (int c =1; c<=N;c++) {
+				col[row] = c;
+				rec_func(row+1);
+				col[row] = 0;
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		input();
+		rec_func(1);
+		System.out.println(ans);
+	}
+
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+	}
+}
