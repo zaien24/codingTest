07ea765b6cd891572f4dcf34892df8c017fc2360
한성길 <zaien24@naver.com>
2025-02-06 11:44:11 +0900

    250206_1144_11720

--- src/doitcoding/P11720_숫자의합_250206.java
+++ src/doitcoding/P11720_숫자의합_250206.java
@@ -1,30 +1,33 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
 	static int N;
-	static String src;
-	static int[] tmp;
+	static String sNum;
+	static char[] cNum;
 
 	public static void proc() {
 		int result = 0;
-		= src.toCharArray();
+		cNum = sNum.toCharArray();
 
-		for (int i = 0; i < tmp.size;) {
-			result += result + tmp[i];
+		for (int i = 0; i < cNum.length; i++) {
+			result += cNum[i] - '0';
 		} 
 
 		System.out.println(result);
 	}
 
-	public static void input() {
-        BufferReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		N = Integer.parseInt(st.nextToken);
+		N = Integer.parseInt(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
-		src = st.nextToken;
+		sNum = st.nextToken();
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		input();
 		proc();
 	}
