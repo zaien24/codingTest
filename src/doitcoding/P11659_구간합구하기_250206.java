b3844e670409a3fbb84978cf1ea79d6f02b1062e
한성길 <zaien24@naver.com>
2025-02-06 14:51:17 +0900

    250206_1451_11659

--- src/doitcoding/P11659_구간합구하기_250206.java
+++ src/doitcoding/P11659_구간합구하기_250206.java
@@ -5,21 +5,10 @@ public class Main {
     static StringBuffer sb = new StringBuffer();
     
     static int N, M; 
-    static int[] A, S;
-
-    public static void proc() {
-        Scanner sc = new Scanner(System.in);
-
-        for (int m = 1; m <= M; m++) {
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-
-            sb.append(S[j] - S[i-1]);
-            sb.append("\n");
-        }
-    }
-
-    public static void input() throws Exception {
+    static int[] A;
+    static long[] S;
+    
+    public static void main(String[] arags) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -27,19 +16,24 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         A = new int[N + 1];
-        S = new int[N];
+        S = new long[N + 1];
         S[0] = 0;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
+        	
             A[i] = Integer.parseInt(st.nextToken());
-            S[i] += A[i];
+            S[i] = S[i-1] + A[i];
         }
-    }
 
-    public static void main(String[] arags) throws Exception {
-        input();    
-        proc();
+        for (int m = 1; m <= M; m++) {
+        	st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(S[j] - S[i-1]);
+            sb.append("\n");
+        }
         System.out.println(sb.toString());
     }
 }
