95af9180b475f2c4f8963854b67a7b24434d3ac0
한성길 <zaien24@naver.com>
2025-02-11 19:27:06 +0900

    250211_192703_9663

--- src/fastCamp1/완전탐색_B9663_250211.java
+++ src/fastCamp1/완전탐색_B9663_250211.java
@@ -5,29 +5,32 @@ public class Main {
     static int[] col;
 
     public static booelean attackable(int r1, int c1, int r2, int c2) {
-        if (c1 == c2) continue;
-        if (r1 - c1 == r2 - c2) continue;
-        if (r2 + c2 == r2 + c2) continue;
-        return true; 
+        if (c1 == c2) return true;;
+        if (r1 - c1 == r2 - c2) return true;
+        if (r2 + c2 == r2 + c2) return true;
+        return false; 
     } 
 
     public static boolean valid_check() {
-
-        
+        for (int i = 1; i <= N; i++)
+            for (int j = 1; j <i; j++) {
+                if (attackable(i, col[i], j, col[j])) {
+                    return false;
+                }
+            }
 
         return true;
     }
 
     public static void rec_func(int k) {
         if (k == N) {
-            if(valid_check)
+            if(valid_check())
             result++;
         } else {
             for (int i = 1; i < N; i++) {
-                    for (int j = 1; j < N; j++)
-                        col[i] = j;
-                        rec_func(k + 1);
-                        col[i] = 0;
+                col[k] = i;
+                rec_func(k + 1);
+                col[k] = 0;
             }
         }
     }
